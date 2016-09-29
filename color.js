832748ed1a5dcc79fec389056dfc00cd344e1b0d
Andy Wolff <andy.wolff@gmail.com>
2016-09-29 13:00:32 -0500

    First Commit.
    This is as far as I could get with this. Was hoping that the switch
    would append colored boxes as it ran through. I looked at and tried a
    number of different way to get to this. I had a random color generator,
    but couldn’t figure out how the box would know what color it was. I
    can’t work on this anymore today (an actual date, no kids with the
    wife, Sigur Ros at the Orpheum!!). I’d like someone to talk a few
    things out with the code if possible.

---
+++ color.js
@@ -0,0 +1,60 @@
+$( document )
+	.ready( function () {
+			var randomNumber;
+			$( '#start' )
+				.click( function () ) {
+					randomNumber = Math.floor( Math.random() * 20 ) + 4;
+				}
+			$( "#box" )
+			switch ( randomNumber ) {
+			case 4:
+				$( '#box' )
+					.append( '<div id="four"></div>' );
+			case 5:
+				$( '#box' )
+					.append( '<div id="five"></div>' );
+			case 7:
+				$( '#box' )
+					.append( '<div id="seven"></div>' );
+			case 8:
+				$( '#box' )
+					.append( '<div id="eight"></div>' );
+			case 9:
+				$( '#box' )
+					.append( '<div id="nine"></div>' );
+			case 10:
+				$( '#box' )
+					.append( '<div id="ten"></div>' );
+			case 11:
+				$( '#box' )
+					.append( '<div id="eleven"></div>' );
+			case 12:
+				$( '#box' )
+					.append( '<div id="twelve"></div>' );
+			case 13:
+				$( '#box' )
+					.append( '<div id="thirteen"></div>' );
+			case 14:
+				$( '#box' )
+					.append( '<div id="fourteen"></div>' );
+			case 15:
+				$( '#box' )
+					.append( '<div id="fifteen"></div>' );
+			case 16:
+				$( '#box' )
+					.append( '<div id="sixteen"></div>' );
+			case 17:
+				$( '#box' )
+					.append( '<div id="seventeen"></div>' )
+			case 18:
+				$( '#box' )
+					.append( '<div id="eighteen"></div>' );
+			case 19:
+				$( '#box' )
+					.append( '<div id="nineteen"></div>' );
+			case 20:
+				$( '#box' )
+					.append( '<div id="twenty"></div>' );
+				break;
+			}
+		}
